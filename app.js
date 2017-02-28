54d46637aba2c2fe77cb82f31e6a63c327ff8d5c
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-02-28 22:11:09 +0700

    express validator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express 	= require('express');
 var bodyParser	= require('body-parser');
 var path		= require('path');
+var expressValidator = require('express-validator');
 
 var app 		= express();
 
@@ -15,12 +16,59 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname,'public')));
 
+//Global variable
+app.use(function(req,res, next){
+	res.locals.errors = null;
+	next();
+});
+
+//express-validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 app.get('/',function(req,res){
 	res.render('index',{
 		judul	: 'Daftar mahasiswa',
 		siswas	: mahasiswa
 	})
+});
+
+app.post('/mahasiswa/tambah', function(req,res){
+
+	req.checkBody('nama','Nama tidak boleh kosong').notEmpty();
+	req.checkBody('npm','NPM tidak boleh kosong').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('index',{
+		judul	: 'Daftar mahasiswa',
+		siswas	: mahasiswa,
+		errors 	: errors
+	})
+	}else{
+		var mahasiswaBaru = {
+			name	: req.body.nama,
+			npm		: req.body.npm
+		}
+		console.log('SUKSES');
+	}
+	
+	
 });
 
 var mahasiswa = [
